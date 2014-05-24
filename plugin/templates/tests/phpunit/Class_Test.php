73217324165442ead687d9bc17162f1d4b2cc9cd
Luke Woodward <luke.woodward@10up.com>
2014-05-23 19:52:59 -0700

    Adding doc comment to example test class

--- plugin/templates/tests/phpunit/Class_Test.php
+++ plugin/templates/tests/phpunit/Class_Test.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * This is a very basic test case to get things started. You should probably rename this and make
+ * it work for your project. You can use all the tools provided by WP Mock and Mockery to create
+ * your tests. Coverage is calculated against your includes/ folder, so try to keep all of your
+ * functional code self contained in there.
+ *
+ * References:
+ *   - http://phpunit.de/manual/current/en/index.html
+ *   - https://github.com/padraic/mockery
+ *   - https://github.com/10up/wp_mock
+ */
+
 use WP_Mock\Tools\TestCase;
 
 class WP_Keen_IO_API_Test extends TestCase {
