0a706205b24dcf7a5d881cd9fc35d12fdcb70af2
Luke Woodward <luke.woodward@10up.com>
2014-05-23 21:11:34 -0700

    Use correct methods for test case class

--- plugin/templates/tests/phpunit/Class_Test.php
+++ plugin/templates/tests/phpunit/Class_Test.php
@@ -14,13 +14,13 @@
 
 use WP_Mock\Tools\TestCase;
 
-class WP_Keen_IO_API_Test extends TestCase {
+class Class_Test extends TestCase {
 
 	/**
 	 * Set up before any test methods run.
 	 */
 	public static function setUpBeforeClass() {
-		\WP_Mock::setUpBeforeClass();
+		parent::setUpBeforeClass();
 		//require_once path/to/tested/file;
 	}
 
@@ -28,7 +28,7 @@ class WP_Keen_IO_API_Test extends TestCase {
 	 * Set up before each test method runs.
 	 */
 	public function setUp() {
-		\WP_Mock::setUp();
+		parent::setUp();
 	}
 
 	/** 
@@ -42,13 +42,13 @@ class WP_Keen_IO_API_Test extends TestCase {
 	 * Clean up run after each test method runs.
 	 */
 	public function tearDown() {
-        \WP_Mock::tearDown();
+        parent::tearDown();
     }
 	
 	/**
 	 * Clean up after all test methods have run.
 	 */
 	public static function tearDownAfterClass() {
-		\WP_Mock::tearDownAfterClass();
+		parent::tearDownAfterClass();
     }
 }
