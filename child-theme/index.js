1c38d496b9d47b814bcfae194845836f20c9633f
Luke Woodward <luke.woodward@10up.com>
2014-05-26 10:02:25 -0700

    Choose a more WordPress-y code style

--- child-theme/index.js
+++ child-theme/index.js
@@ -1,39 +1,39 @@
 'use strict';
-var util = require('util');
-var path = require('path');
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var async = require('async');
+var util = require( 'util' );
+var path = require( 'path' );
+var yeoman = require( 'yeoman-generator' );
+var chalk = require( 'chalk' );
+var async = require( 'async' );
 
 
-var ChildThemeGenerator = yeoman.generators.Base.extend({
+var ChildThemeGenerator = yeoman.generators.Base.extend( {
 	init: function () {
-		this.log(chalk.magenta('Thanks for generating with WP Make!'));
+		this.log( chalk.magenta( 'Thanks for generating with WP Make!' ) );
 
-		this.on('end', function () {
+		this.on( 'end', function () {
 			var i, length, installs = [],
-				chalks = {skipped:[], run:[]},
-				installers = ['npm', 'bower', 'composer'];
+				chalks = { skipped:[], run:[] },
+				installers = [ 'npm', 'bower', 'composer' ];
 
-			this.log(chalk.green.bold('Your child-theme has been generated.'));
+			this.log( chalk.green.bold( 'Your child-theme has been generated.' ) );
 
-			for (i = 0, length = installers.length; i < length; i++) {
-				if (this.options['skip-install'] || this.options[ 'skip-' + installers[ i ] ] ) {
-					chalks.skipped.push(chalk.yellow.bold(installers[ i ] + ' install'));
+			for ( i = 0, length = installers.length; i < length; i++ ) {
+				if ( this.options['skip-install'] || this.options[ 'skip-' + installers[ i ] ] ) {
+					chalks.skipped.push( chalk.yellow.bold( installers[ i ] + ' install' ) );
 				} else {
-					chalks.run.push(chalk.yellow.bold(installers[ i ] + ' install'));
-					installs.push(_install(installers[ i ],this));
+					chalks.run.push( chalk.yellow.bold( installers[ i ] + ' install' ) );
+					installs.push( _install( installers[ i ],this ) );
 				}
 			}
 			
 			if ( 0 < chalks.skipped.length ) {
-				this.log( 'Skipping ' + chalks.skipped.join(', ') + '. Just run yourself when you are ready.');
+				this.log( 'Skipping ' + chalks.skipped.join( ', ' ) + '. Just run yourself when you are ready.' );
 			}
 			if ( 0 < installs.length ) {
-				this.log( 'Running ' + chalks.run.join(', ') + ' for you. If this fails try running yourself.');
-				async.parallel(installs);
+				this.log( 'Running ' + chalks.run.join( ', ' ) + ' for you. If this fails try running yourself.' );
+				async.parallel( installs );
 			}
-		});
+		} );
 	},
 
 	options: function () {
@@ -52,7 +52,7 @@ var ChildThemeGenerator = yeoman.generators.Base.extend({
 			},
 			{
 				name:    'funcPrefix',
-				message: 'PHP function prefix (lowercase letters and underscores only)',
+				message: 'PHP function prefix ( lowercase letters and underscores only )',
 				default: 'wptheme'
 			},
 			{
@@ -87,14 +87,14 @@ var ChildThemeGenerator = yeoman.generators.Base.extend({
 			}
 		];
 		// gather initial settings
-		this.prompt(prompts, function (props) {
+		this.prompt( prompts, function ( props ) {
 			this.opts = props;
-			this.fileSlug = this.opts.projectTitle.toLowerCase().replace(/[\s]/g, '-').replace( /[^a-z-_]/g, '' );
-			this.classSlug = this.fileSlug.replace( /-/g, '_' ).replace( /(^|_)([a-z])/g, function( match, group1, group2 ){
+			this.fileSlug = this.opts.projectTitle.toLowerCase().replace( /[\s]/g, '-' ).replace( /[^a-z-_]/g, '' );
+			this.classSlug = this.fileSlug.replace( /-/g, '_' ).replace( /( ^|_ )( [a-z] )/g, function( match, group1, group2 ){
 				return group1 + group2.toUpperCase(); 
-			});
+			} );
 			done();
-		}.bind(this));
+		}.bind( this ) );
 	},
 
 	autoprefixer: function() {
@@ -106,16 +106,16 @@ var ChildThemeGenerator = yeoman.generators.Base.extend({
 
 		// See if we want to use it on it's own, but only if not using Sass.
 		var done = this.async();
-		this.prompt([{
+		this.prompt( [{
 			type:    'confirm',
 			name:    'autoprefixer',
 			message: 'Use Autoprefixer?',
 			default: true
 		}],
-		function(props){
+		function( props ){
 			this.opts.autoprefixer = props.autoprefixer;
 			done();
-		}.bind(this));
+		}.bind( this ) );
 	},
 
 	theme: function() {
@@ -180,20 +180,20 @@ var ChildThemeGenerator = yeoman.generators.Base.extend({
 	git: function() {
 		this.copy( 'git/gitignore', '.gitignore' );
 	}
-});
+} );
 
 function _install( command, context ) {
 	return function install( cb ) {
-		context.emit(command + 'Install');
+		context.emit( command + 'Install' );
 		context.spawnCommand( command, ['install'] )
-		.on('error', cb)
-		.on('exit', context.emit.bind(context, command + 'Install:end'))
-		.on('exit', function (err) {
-			if (err === 127) {
-				this.log.error('Could not find Composer');
+		.on( 'error', cb )
+		.on( 'exit', context.emit.bind( context, command + 'Install:end' ) )
+		.on( 'exit', function ( err ) {
+			if ( err === 127 ) {
+				this.log.error( 'Could not find Composer' );
 			}
-			cb(err);
-		}.bind(context));
+			cb( err );
+		}.bind( context ) );
 	}
 }
 
