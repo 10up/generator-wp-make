de297df2b0880dfbcc9c120dca516ce0084ae9b1
Eric Mann <eric@eamann.com>
2016-03-23 09:24:05 -0700

    es6ify

--- yo-profile/src/yo-profile.js
+++ yo-profile/src/yo-profile.js
@@ -1,32 +1,47 @@
 'use strict';
-var _ = require( 'lodash' ),
-	rc = require( 'rc' );
+import _ from 'lodash';
+import rc from 'rc';
 
 // Specify defaults - all are `undefined` to flag they don't exist
 var options = {
-	'license'  : undefined,
-	'humanstxt': undefined,
-	'namespace': undefined,
-	'php_min'  : undefined,
-	'wp_tested': undefined,
-	'wp_min'   : undefined
+	'license'       : undefined,
+	'humanstxt'     : undefined,
+	'root_namespace': undefined,
+	'php_min'       : undefined,
+	'wp_tested'     : undefined,
+	'wp_min'        : undefined
 };
 
-// Parse the stored config
-var config = rc( 'wpmake' );
+class YoProfile {
+	constructor() {
+		this._props = {};
+	}
 
-var profile = undefined;
-if ( undefined !== config.profile ) {
-	// The profile is explicitly set
-	profile = config.profile;
-} else if ( undefined !== config.default ) {
-	profile = config.default;
+	load( defaults ) {
+		// Parse the stored config
+		var config = rc( 'wpmake' );
+
+		var profile = undefined;
+		if ( undefined !== config.profile ) {
+			// The profile is explicitly set
+			profile = config.profile;
+		} else if ( undefined !== config.default ) {
+			profile = config.default;
+		}
+
+		// If we're specifying a profile, fetch that profile
+		if ( undefined !== profile && undefined !== config[ profile ] ) {
+			_.extend( defaults, config[ profile ] );
+		}
+
+		this._props = defaults;
+
+		return this;
+	}
+
+	get properties() {
+		return this._props;
+	}
 }
 
-// If we're specifying a profile, fetch that profile
-if ( undefined !== profile && undefined !== config[ profile ] ) {
-	_.extend( options, config[ profile ] );
-}
-
-// Export the final object
-module.exports = options;
+export default YoProfile;
