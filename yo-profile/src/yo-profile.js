ef39e2280d99a161b261deaa9ce5b845e23c2a59
Eric Mann <eric@eamann.com>
2016-03-23 09:32:03 -0700

    Polish documentation

--- yo-profile/src/yo-profile.js
+++ yo-profile/src/yo-profile.js
@@ -2,26 +2,34 @@
 import _ from 'lodash';
 import rc from 'rc';
 
-// Specify defaults - all are `undefined` to flag they don't exist
-var options = {
-	'license'       : undefined,
-	'humanstxt'     : undefined,
-	'root_namespace': undefined,
-	'php_min'       : undefined,
-	'wp_tested'     : undefined,
-	'wp_min'        : undefined
-};
-
+/**
+ * Object representing a profile loader
+ */
 class YoProfile {
+	/**
+	 * Instantiate the object and set the internal collection with an empty object
+	 */
 	constructor() {
 		this._props = {};
 	}
 
-	load( defaults ) {
-		// Parse the stored config
-		var config = rc( 'wpmake' );
+	/**
+	 * Load the actual rc file into memory and parse it with specific defaults.
+	 *
+	 * @param {Object} defaults Default profile properties
+	 * @param {String} [file]   Optional file name - defaults to `yoprofile` if not specified
+	 *
+	 * @returns {YoProfile}
+	 */
+	load( defaults, file ) {
+		if ( undefined === file ) {
+			file = 'yoprofile';
+		}
 
-		var profile = undefined;
+		// Parse the stored config
+		let config = rc( file );
+
+		let profile;
 		if ( undefined !== config.profile ) {
 			// The profile is explicitly set
 			profile = config.profile;
@@ -36,12 +44,19 @@ class YoProfile {
 
 		this._props = defaults;
 
+		// Return the current instance for chaining
 		return this;
 	}
 
+	/**
+	 * Define a property fetcher to expose the internal collection.
+	 *
+	 * @returns {Object}
+	 */
 	get properties() {
 		return this._props;
 	}
 }
 
+// Export the module
 export default YoProfile;
