763c9fe1e6ac7212109f9d7a7dcae7812875f66a
Eric Mann <eric@eamann.com>
2016-03-22 17:36:26 -0700

    Actually parse profiles and populate options

--- yo-profile/src/yo-profile.js
+++ yo-profile/src/yo-profile.js
@@ -1,4 +1,32 @@
 'use strict';
-var rc = require( 'rc' );
+var _ = require( 'lodash' ),
+	rc = require( 'rc' );
 
-module.exports = {};
+// Specify defaults - all are `undefined` to flag they don't exist
+var options = {
+	'license'  : undefined,
+	'humanstxt': undefined,
+	'namespace': undefined,
+	'php_min'  : undefined,
+	'wp_tested': undefined,
+	'wp_min'   : undefined
+};
+
+// Parse the stored config
+var config = rc( 'wpmake' );
+
+var profile = undefined;
+if ( undefined !== config.profile ) {
+	// The profile is explicitly set
+	profile = config.profile;
+} else if ( undefined !== config.default ) {
+	profile = config.default;
+}
+
+// If we're specifying a profile, fetch that profile
+if ( undefined !== profile && undefined !== config[ profile ] ) {
+	_.extend( options, config[ profile ] );
+}
+
+// Export the final object
+module.exports = options;
