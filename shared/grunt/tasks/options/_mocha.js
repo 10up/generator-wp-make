a7a8e63cc97b643946a87eb40a35cc34d01d47f6
Allen Moore <allen.moore@10up.com>
2016-03-31 12:06:37 -0400

    remove php file option from mocha src

--- shared/grunt/tasks/options/_mocha.js
+++ shared/grunt/tasks/options/_mocha.js
@@ -2,7 +2,7 @@ var mochaPath = 'tests/mocha/';
 
 module.exports = {
 	test: {
-		src: [ mochaPath + '**/*.php', mochaPath + '**/*.html' ],
+		src: [ mochaPath + '**/*.html' ],
 		options: {
 			run: true,
 			timeout: 10000
