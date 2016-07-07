5a9ad9782f211291544e68fd2f1797986f5d3986
Nicholas Smith <nicholasparkersmith@gmail.com>
2016-07-07 10:31:13 -0400

    Adding in tests for util>tree::getSubTree function
    These tests cover the use cases that I encountered this week as I tried
    to modify this application. Hopefully they'll be abel to be a little
    instructive as to how these functions works as well as confirming that
    the functionality I flushed out earlier this week remains the same.

---
+++ test/lib/util/tree.js
@@ -0,0 +1,49 @@
+'use strict';
+var assert = require('chai').assert;
+var tree = require( '../../../lib/util/tree' );
+
+describe('lib > util > tree', function () {
+	describe('Setup', function () {
+		it('can be imported', function () {
+			assert.isOk(tree, 'tree is available');
+		});
+		it('functions exist', function () {
+			assert.isFunction(tree.tree, 'tree.tree is a function');
+			assert.isFunction(tree.getSubtree, 'tree.getSubtree is a function');
+		});
+	});
+	describe('getSubtree()', function () {
+		beforeEach(function() {
+			tree.lifecycle = {
+				tree: {
+					tree: {
+						'foo': {
+							tree: {
+								'bar': {
+									'baz': 'qux'
+								}
+							}
+						}
+					},
+					quux : 'corge'
+				}
+			};
+		});
+		it('get first child', function () {
+			var expected = 'corge',
+				actual = tree.getSubtree( 'quux' );
+			assert.equal(actual, expected, 'retrieve first child successfully');
+		});
+		it('get second child', function () {
+			var expected = 'qux',
+				actual = tree.getSubtree( 'baz', 'foo/bar' );
+				assert.equal(actual, expected, 'retrieve second child successfully');
+		});
+		it('create a child', function () {
+			var expected = {'foo': 'bar'},
+				actual = tree.getSubtree( 'bar', 'foo/baz' );
+				actual['foo'] = 'bar';
+				assert.deepEqual(actual, expected, 'create a child successfully');
+		});
+	});
+});
