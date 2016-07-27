94ae17b45490534088a304e909be33f93b9cf2d6
Kyle Reicks <kyle.reicks@10up.com>
2016-07-27 14:17:54 -0500

    Add back licence comment. Add comments for initialization events.

--- shared/js/_script.js
+++ shared/js/_script.js
@@ -1,11 +1,22 @@
+/**
+ * <%= opts.projectTitle %>
+ * <%= opts.projectHome %>
+ *
+ * Copyright (c) <%= new Date().getFullYear() %> <%= opts.authorName %>
+ * <% if ( opts.license ) { %>Licensed under the <%= opts.license %> license.<% } %>
+ */
+// Import dependencies.
 import component from './component/component';
 
 if ( window.addEventListener ) {
+	// Initiate our script once the Document Object Model is set.
 	window.addEventListener( 'DOMContentLoaded', component, false );
 } else if ( window.attachEvent ) {
+	// Internet Explorer fallback. Initiate our script 'onload'.
 	window.attachEvent( 'onload', component );
 }
 
+// Initiate our script if it is loaed after the Document ready state is already complete.
 if ( 'complete' === document.readyState ) {
 	component();
 }
