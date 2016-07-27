c6d190db0dae5b3a5f149e671bcfb8a3633193d8
Kyle Reicks <kyle.reicks@10up.com>
2016-07-27 13:42:01 -0500

    Better default start. onload for IE and readystate complete for asynchronus loading.

--- shared/js/_script.js
+++ shared/js/_script.js
@@ -3,5 +3,9 @@ import component from './component/component';
 if ( window.addEventListener ) {
 	window.addEventListener( 'DOMContentLoaded', component, false );
 } else if ( window.attachEvent ) {
-	window.attachEvent( 'DOMContentLoaded', component );
+	window.attachEvent( 'onload', component );
+}
+
+if ( 'complete' === document.readyState ) {
+	component();
 }
