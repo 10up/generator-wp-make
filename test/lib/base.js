75b5a2e89d6ee2bacedcdd9696b22ed696549601
Nicholas Smith <nicholasparkersmith@gmail.com>
2016-08-12 14:45:46 -0400

    round out for the last files that include functional programming to be tested

---
+++ test/lib/base.js
@@ -0,0 +1,14 @@
+'use strict';
+var assert = require('chai').assert;
+var base   = require('../../lib/base');
+
+describe('lib > base', function () {
+	/**
+	 * Confirm the file is loading correctly and tested functions are available
+	 */
+	describe('Setup', function () {
+		it('can be imported', function () {
+			assert.isOk(base, 'base is available');
+		});
+	});
+});
