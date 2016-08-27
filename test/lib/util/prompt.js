c9f1ed0fc014dab5ff08b69fe2d3dfc607262f5f
Luke Woodward <woodward.lucas@gmail.com>
2016-08-27 14:59:56 -0500

    Start adding functional tests for prompts

--- test/lib/util/prompt.js
+++ test/lib/util/prompt.js
@@ -13,4 +13,39 @@ describe('lib > util > prompt', function () {
 			assert.isFunction(prompt.prompt, 'prompt.prompt is a function');
 		});
 	});
+	describe('the prompt method will compile question answers', function () {
+		it('runs each question individually, gathering answers', function() {
+			let count = 0;
+			const context = {};
+			const mockQuestions = [
+				{
+					name:    'question1',
+					message: 'Question 1'
+				},
+				{
+					name:    'question2',
+					message: 'Question 2'
+				},
+				{
+					name:    'question3',
+					message: 'Question3'
+				},
+			];
+			// Mocks the call to the yeoman prompt prototype.
+			function mockPrompt( question ) {
+				// Increment call count.
+				count++;
+				// Check to ensure we got passed the question object.
+				assert.equal( question, mockQuestions[count] );
+				// Check to ensure context is set to the generator.
+				assert.equals(this, context);
+				// return the incremented answer.
+				return `answer${count}`;
+			}
+			prompt.prompt.call( context, mockQuestions, {}, mockPrompt )
+				.then((answers) => (
+					assert.isTrue(answers);
+				));
+		});
+	})
 });
