9bf81c2e73d59f74c52659323c794bdb42c4d8ed
Luke Woodward <woodward.lucas@gmail.com>
2016-10-07 19:40:16 -0500

    test the tools functionality

--- src/util/tools.js
+++ src/util/tools.js
@@ -6,11 +6,38 @@
 import path from 'path';
 
 /**
- * Sets up a node dependency in package.json
+ * A generic JSON manager for dependency manager JSON dependency definitions.
  *
- * If package.json has not be defined in the lifecycle tree, it will create it.
- * The package will then be added to the npm dependencies as either a normal or
- * devDependency depending on the dev flag passed.
+ * If the JSON file is not defined in the lifecycle tree, it will create it. The
+ * dependency will then be added to the JSON definition based on the dev flag
+ * and the passed devDep/normalDep keys. This works for composer, npm, and any
+ * other JSON definition that follows this type of pattern.
+ *
+ * @param {Object} keys    The object defining the specific type of JSON file.
+ * @param {String} name    The name of the dependency to add.
+ * @param {String} version The version string to use for the dependency.
+ * @param {Bool}   dev     Whether this should go in dev or normal dependencies.
+ */
+export function jsonDependency ( keys, name, version, dev ) {
+	const type = dev ? keys.devDep : keys.normalDep;
+	const rootJSON = this.getSubtree( 'json' );
+
+	if ( ! rootJSON[ keys.type ] ) {
+		rootJSON[ keys.type ] = this.starterJSON( keys.starter );
+	}
+
+	if ( ! rootJSON[ keys.type ] ) {
+		rootJSON[ keys.type ][ type ] = {};
+	}
+
+	rootJSON[ keys.type ][ type ][ name ] = version;
+}
+
+/**
+ * Runs the jsonDependency method for npm specifically.
+ *
+ * Sets the type to package.json, normal/dev dependencies to dependencies and
+ * devDependencies specifically, and the starter to 'package'.
  *
  * @param  {string} name    The name of the npm package to add.
  * @param  {String} version The semver string to use in package.json.
@@ -18,26 +45,19 @@ import path from 'path';
  * @return {void}
  */
 export function nodeDependency ( name, version, dev ) {
-	const type = ! dev ? 'dependencies' : 'devDependencies';
-	const rootJSON = this.getSubtree( 'json' );
-
-	if ( ! rootJSON['package.json'] ) {
-		rootJSON['package.json'] = this.starterJSON( 'package' );
-	}
-
-	if ( ! rootJSON['package.json'][ type ] ) {
-		rootJSON['package.json'][ type ] = {};
-	}
-
-	rootJSON['package.json'][ type ][ name ] = version;
+	this.jsonDependency( {
+		type: 'package.json',
+		starter: 'package',
+		normalDep: 'dependencies',
+		devDep: 'devDependencies'
+	}, name, version, dev );
 }
 
 /**
- * Sets up a composer dependency in composer.json
+ * Runs the jsonDependency method for composer specifically.
  *
- * If composer.json has not be defined in the lifecycle tree, it will create it.
- * The package will then be added to the composer dependencies as either a
- * normal or dev requirement depending on the dev flag passed.
+ * Sets the type to composer.json, normal/dev dependencies to require and
+ * require-dev specifically, and the starter to 'composer'.
  *
  * @param  {string} name    The name of the composer package to add.
  * @param  {String} version The semver string to use in composer.json.
@@ -45,18 +65,12 @@ export function nodeDependency ( name, version, dev ) {
  * @return {void}
  */
 export function composerDependency ( name, version, dev ) {
-	const type = ! dev ? 'require' : 'require-dev';
-	const rootJSON = this.getSubtree( 'json' );
-
-	if ( ! rootJSON['composer.json'] ) {
-		rootJSON['composer.json'] = this.starterJSON( 'composer' );
-	}
-
-	if ( ! rootJSON['composer.json'][ type ] ) {
-		rootJSON['composer.json'][ type ] = {};
-	}
-
-	rootJSON['composer.json'][ type ][ name ] = version;
+	this.jsonDependency( {
+		type: 'composer.json',
+		starter: 'composer',
+		normalDep: 'require',
+		devDep: 'require-dev'
+	}, name, version, dev );
 }
 
 /**
@@ -87,7 +101,7 @@ export function gruntConfig ( task, config ) {
  *
  * @param  {String} template The name of the starter template to retrieve.
  * @param  {String} type     The extension of the starter template.
- * @return {String}          The strinified file contents, or empty.
+ * @return {String}          The stringified file contents, or empty.
  */
 export function starter ( template, type = 'js' ) {
 	const templateFile = 'starters/_' + template + '.' + type;
@@ -126,6 +140,7 @@ export function starterJSON ( template ) {
 export default {
 	nodeDependency,
 	composerDependency,
+	jsonDependency,
 	gruntConfig,
 	starter,
 	starterJSON
--- test/lib/util/tools.js
+++ test/lib/util/tools.js
@@ -1,44 +1,15 @@
-var assert = require('chai').assert;
-var mock = require('mock-fs');
-var path = require('path');
-var tools = require('../../../lib/util/tools');
+/**
+ * This file contains tests for the tools helper methods.
+ */
 
+// Import dependencies
+import {assert} from 'chai';
+import tools from '../../../lib/util/tools';
+
+/**
+ * Run tests on the tools helper methods.
+ */
 describe('lib > util > tools', function () {
-	/**
-	 * Setup the entire stuite of tests with base mocked params
-	 */
-	before(function() {
-		var sanitizedFiles = {
-			starters: {
-				'_foo.js': 'bar',
-			}
-		};
-		tools.fs = mock.fs(sanitizedFiles);
-		tools.fs.exists = function(param) {
-			var truthy = false;
-			Object.keys(sanitizedFiles.starters).map(function(k) {
-				var test = k.split('.');
-				if (param.indexOf(test[0]) !== -1) {
-					truthy = true;
-				}
-			});
-			return truthy;
-		};
-		tools.fs.read = function(param) {
-			return param;
-		};
-	});
-	after(mock.restore);
-
-	/**
-	 * Clean up the mock for each test
-	 */
-	beforeEach(function() {
-		tools.templatePath = function(param) {
-			return param;
-		};
-	});
-
 	/**
 	 * Confirm the file is loading correctly and tested functions are available
 	 */
@@ -47,47 +18,304 @@ describe('lib > util > tools', function () {
 			assert.isOk(tools, 'tools is available');
 		});
 		it('functions exist', function () {
-			assert.isFunction(tools.starter, 'tools.starter is a function');
+			assert.isFunction(tools.starter, 'tools.starter is a function' );
+			assert.isFunction(tools.jsonDependency, 'tools.jsonDependency is a function.');
+			assert.isFunction(tools.nodeDependency, 'tools.nodeDependency is a function.');
+			assert.isFunction(tools.composerDependency, 'tools.composerDependency is a function.');
+			assert.isFunction(tools.gruntConfig, 'tools.gruntConfig is a function.');
+			assert.isFunction(tools.starterJSON, 'tools.starterJSON is a function.');
 		});
 	});
-
 	/**
-	 * Test all aspects of the starter function that is the backbone of the
-	 * application functionality found in tool library
+	 * Test the nodeDependency method to ensure it works as epctected.
 	 */
-	describe('starter()', function () {
-		it('template does not exist', function() {
-			var expected = '';
-			// Create random file name
-			var actual = tools.starter( Math.random().toString(36).substring(7) );
-			assert.equal(actual, expected, 'File does not exist');
+	describe('#jsonDependency', function () {
+		before( function () {
+			this.gst = tools.getSubtree;
+			tools.getSubtree = () => this.gstReturn;
 		});
-		it('local template does exist', function() {
-			var expected = 'starters/_foo.js';
-			var actual = tools.starter( 'foo' );
-			assert.equal(actual, expected, 'File does not exist');
+		beforeEach( function () {
+			this.gstReturn = {};
 		});
-		it('local template does exist with new end param', function() {
-			var expected = 'starters/_foo.bar';
-			var actual = tools.starter( 'foo', 'bar' );
-			assert.equal(actual, expected, 'File does not exist');
+		after( function () {
+			tools.getSubtree = this.gst;
+			delete this.gstReturn;
 		});
-		it('base template does exist', function() {
-			tools.templatePath = function() {
-				return '';
+		it( 'will add a dependency to the root json object', function () {
+			// Set up mocks
+			this.gstReturn = {
+				'testing.json': {
+					normal: {}
+				}
 			};
-			var expected = path.join( __dirname, '../../../lib', 'starters/_foo.js' );
-			var actual = tools.starter( 'foo' );
-			assert.equal(actual, expected, 'File does not exist');
-		});
-		it('base template does exist with new end param', function() {
-			tools.templatePath = function() {
-				return '';
+			// Run the test
+			tools.jsonDependency(
+				{
+					type: 'testing.json',
+					normalDep: 'normal'
+				},
+				'fake',
+				'1.0.0'
+			);
+			// Verify the restult
+			assert.deepEqual(
+				this.gstReturn['testing.json'].normal,
+				{ fake: '1.0.0' }
+			);
+		} );
+		it( 'will add a dev dependency to the root json object', function () {
+			// Set up mocks
+			this.gstReturn = {
+				'testing.json': {
+					dev: {}
+				}
 			};
-			var expected = path.join( __dirname, '../../../lib', 'starters/_foo.bar' );
-			var actual = tools.starter( 'foo', 'bar' );
-			assert.equal(actual, expected, 'File does not exist');
+			// Run the test
+			tools.jsonDependency(
+				{
+					type: 'testing.json',
+					devDep: 'dev'
+				},
+				'fake',
+				'1.0.0',
+				true
+			);
+			// Verify the restult
+			assert.deepEqual(
+				this.gstReturn['testing.json'].dev,
+				{ fake: '1.0.0' }
+			);
+		} );
+		it( 'will create objects missing in the tree', function () {
+			// Create mocks
+			this.starterJSON = tools.starterJSON;
+			tools.starterJSON = () => ({normal: {}});
+			// Run the test
+			tools.jsonDependency(
+				{
+					type: 'testing.json',
+					normalDep: 'normal'
+				},
+				'fake',
+				'1.0.0'
+			);
+			// Verify the restult
+			assert.isObject(this.gstReturn['testing.json']);
+			assert.isObject(this.gstReturn['testing.json'].normal);
+			assert.deepEqual(
+				this.gstReturn['testing.json'].normal,
+				{fake: '1.0.0'}
+			);
+			// Restore mocks
+			tools.starterJSON = this.starterJSON;
+			delete this.starterJSON;
+		} );
+	} );
+	/**
+	 * Check the nodeDependency method to ensure it calls jsonDependency right.
+	 */
+	describe('#nodeDependency', function () {
+		it('will call #jsonDependency with package.json', function () {
+			// Set up mocks
+			this.jsonDependency = tools.jsonDependency;
+			tools.jsonDependency = ( ...val ) => { this.depVal = val; };
+			// Run the test.
+			tools.nodeDependency( 'test', '1.0.0', false );
+			// Verify
+			assert.deepEqual(
+				this.depVal,
+				[
+					{
+						type: 'package.json',
+						starter: 'package',
+						normalDep: 'dependencies',
+						devDep: 'devDependencies'
+					},
+					'test',
+					'1.0.0',
+					false
+				]
+			);
+			// Restore
+			tools.jsonDependency = this.jsonDependency;
+			delete this.jsonDependency;
+			delete this.depVal;
 		});
-
+	});
+	/**
+	 * Check composerDependency method to ensure it calls jsonDependency right.
+	 */
+	describe('#composerDependency', function () {
+		it('will call #jsonDependency with composer.json', function () {
+			// Set up mocks
+			this.jsonDependency = tools.jsonDependency;
+			tools.jsonDependency = ( ...val ) => { this.depVal = val; };
+			// Run the test.
+			tools.composerDependency( 'test', '1.0.0', false );
+			// Verify
+			assert.deepEqual(
+				this.depVal,
+				[
+					{
+						type: 'composer.json',
+						starter: 'composer',
+						normalDep: 'require',
+						devDep: 'require-dev'
+					},
+					'test',
+					'1.0.0',
+					false
+				]
+			);
+			// Restore
+			tools.jsonDependency = this.jsonDependency;
+			delete this.jsonDependency;
+			delete this.depVal;
+		});
+	});
+	/**
+	 * Test the gruntConfig method to ensure it adds configs to the tree.
+	 */
+	describe('#gruntConfig', function () {
+		it('will add a task config to the grunt task output tree', function () {
+			// Set up mocks
+			this.mockTree = {};
+			tools.getSubtree = (...args) => {
+				assert.deepEqual(
+					args,
+					['modules', 'tasks']
+				);
+				return this.mockTree;
+			};
+			// Run the test.
+			tools.gruntConfig( 'test', { def: 'thing' } );
+			// Verify
+			assert.deepEqual(
+				this.mockTree,
+				{
+					'test.js': { def: 'thing' }
+				}
+			);
+			// Restore
+			delete tools.getSubtree;
+			delete this.mockTree;
+		});
+	});
+	/**
+	 * Test all aspects of the starter function.
+	 *
+	 * This method is crucial to the behavior of this application.
+	 */
+	describe('#starter', function () {
+		afterEach(function () {
+			// Reset the mocks
+			delete tools.templatePath;
+			delete tools.fs;
+		});
+		it('template does not exist it will throw an error', function() {
+			// Create mocks
+			// When FS can't read a file, it errors, we'll just mock it always
+			// throwing erros to simulate no files reachable.
+			tools.templatePath = () => null;
+			tools.fs = {
+				read: () => { throw new Error('die'); }
+			};
+			// Run the test.
+			assert.throws(
+				() => { tools.starter('testing'); },
+				'Unable to locate starters/_testing.js.'
+			);
+		});
+		it('sends back the file if found in the templatePath', function() {
+			// Create mocks
+			// Just pass everything through so the files contents is
+			// the file name.
+			tools.templatePath = val => val;
+			tools.fs = {
+				read: val => val
+			};
+			// Run the test.
+			assert.equal(tools.starter('testing'), 'starters/_testing.js');
+		});
+		it('sends back the global template if local is not found', function () {
+			// Create mocks
+			// The local request, call error, otherwise do a passthrough.
+			tools.templatePath = val => val;
+			tools.fs = {
+				read: val => {
+					if (val === 'starters/_testing.js'){
+						throw new Error( 'die' );
+					} else {
+						return val;
+					}
+				}
+			};
+			// Rund the test
+			assert.equal(tools.starter('global'), 'starters/_global.js');
+		});
+		it('allows overriding the extension', function() {
+			// Create mocks
+			// Just pass everything through so the files contents is
+			// the file name.
+			tools.templatePath = val => val;
+			tools.fs = {
+				read: val => val
+			};
+			// Run the test.
+			assert.equal(
+				tools.starter('testing', 'json'),
+				'starters/_testing.json'
+			);
+		});
+	});
+	/**
+	 * Make sure the starterJSON method properly wraps the starter method.
+	 */
+	describe('#starterJSON', function () {
+		before(function () {
+			this.starter = tools.starter;
+			tools.starter = ( ...args ) => {
+				this.starterArgs = args;
+				return this.starterReturn;
+			};
+		});
+		beforeEach(function () {
+			this.starterArgs = undefined;
+			this.starterReturn = undefined;
+		});
+		after(function () {
+			tools.starter = this.starter;
+			delete this.starterArgs;
+			delete this.starterReturn;
+		});
+		it('decodes valid JSON that is returned', function () {
+			// Set up mocks.
+			this.starterReturn = '{ "testing": true }';
+			// Run test and validate
+			assert.isTrue( tools.starterJSON( 'testing' ).testing );
+		});
+		it('sends back an empty object when the startes is empty', function () {
+			// Set up mocks.
+			this.starterReturn = '';
+			// Run test
+			const result = tools.starterJSON( 'testing' );
+			// Verify results
+			assert.isObject( result );
+			assert.deepEqual( result, {} );
+		});
+		it('throws an error on an invalid JSON', function () {
+			// Set up mocks.
+			// Valid JSON requires double quotes.
+			this.starterReturn = '{ testing: true }';
+			// Run test and validate
+			assert.throws(() => { tools.starterJSON('testing'); });
+		});
+		it('send JSON as the type to the starter method', function () {
+			// Set up mocks
+			this.starterReturn = '';
+			// Run test
+			tools.starterJSON('testing');
+			assert.deepEqual( this.starterArgs, ['testing', 'json'] );
+		});
 	});
 });
